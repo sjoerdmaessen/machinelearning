5e8a9d12712fcc88c878c9653a136e2ab36c0b0c
Sjoerd Maessen <smaessen@Apple.local>
2016-05-04 20:23:33 +0200

    Support scaling multiple values at once

--- src/Feature/Preprocessor/Scale.php
+++ src/Feature/Preprocessor/Scale.php
@@ -9,10 +9,24 @@ class Scale
 {
     /**
      * Rescales a number from one scale to a number on another scale
+     *
+     * @param array|float|int $input
+     * @param int $currentMin
+     * @param int $currentMax
+     * @param int $newMin
+     * @param int $newMax
      */
-    static public function rescale($value, $currentMin = 0, $currentMax = 100, $newMin = 0, $newMax = 1)
+    static public function rescale($input, $currentMin = 0, $currentMax = 100, $newMin = 0, $newMax = 1)
     {
-        return $newMin + ($newMax - $newMin) * (($value - $currentMin) / ($currentMax - $currentMin));
+        if (is_array($input)) {
+            foreach ($input as &$value) {
+                $value = $newMin + ($newMax - $newMin) * (($value - $currentMin) / ($currentMax - $currentMin));
+            }
+        } else {
+            $input = $newMin + ($newMax - $newMin) * (($input - $currentMin) / ($currentMax - $currentMin));
+        }
+
+        return $input;
     }
 
 }
